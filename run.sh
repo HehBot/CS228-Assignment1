7f4d8ab2179f42b87b8cf4e6637494c9df7a37c6
HehBot <razor7827@gmail.com>
2023-03-06 14:33:25 +0530

    Cleaned up run.sh
    See `./run.sh -h`

--- run.sh
+++ run.sh
@@ -1,56 +1,61 @@
 #!/usr/bin/bash
 
-if [[ "$1" = "-h" ||  ! ( $# -eq 0 || $# -eq 1 || $# -eq 3 ) ]]; then
-    Z="test/input.txt test/specimen.txt"
-    echo "Usage 1: $0"
-    echo "    Uses existing testcase in $Z"
+if [[ "$1" = "-h" || ! ( $# -eq 1 || $# -eq 4 ) ]]; then
+    Z="<dir>/input.txt <dir>/specimen.txt"
+    echo "USAGE:"
     echo
-    echo "Usage 2: $0 <dir>"
-    echo "    Uses existing testcase in <dir>/input.txt <dir>/specimen.txt"
+    echo "    $0 <dir>"
+    echo "        Uses existing testcase in $Z"
     echo
-    echo "Usage 3: $0 <N> <T> <sat|unsat>"
-    echo "    Generates and uses new testcase in $Z"
+    echo "    $0 <dir> <N> <T> <sat|unsat>"
+    echo "        Generates and uses new testcase in $Z"
+    echo
     exit
 fi
 
 PY=python3
 
-TEST=test
+TEST=$1
 if [ $# -eq 1 ]; then
-    if [ -e $1/input.txt ]; then
-        TEST=$1
-    else
+    if ! [ -e $1/input.txt ]; then
         echo "Invalid test files in dir \"$1\""
         exit
     fi
+else
+    rm -rf $TEST
+    mkdir -p $TEST
 fi
 
 INPUT=$TEST/input.txt
 OUTPUT=$TEST/output.txt
 SPECIMEN=$TEST/specimen.txt
 
-mkdir -p $TEST
-
 CODE=210050018_210050038_210050085_tile_loop.py
 
-if [ $# -eq 3 ]; then
-    $PY generator.py $1 $2 $3 $INPUT | tail -n +3 > $SPECIMEN
+if [ $# -eq 4 ]; then
+    $PY generator.py $2 $3 $4 $INPUT | tail -n +3 > $SPECIMEN
 fi
 
 time $PY $CODE $INPUT > $OUTPUT
 
 X=$(head -1 $OUTPUT)
-Y=$(cat $SPECIMEN)
-if [ "$Y" = "" ]; then
-    Y="unsat"
-else
-    Y="sat"
+Y="no_specimen"
+if [ -e $SPECIMEN ]; then
+    if [ "$(cat $SPECIMEN)" = "" ]; then
+        Y="unsat"
+    else
+        Y="sat"
+    fi
 fi
-if ! [ "$X" = "$Y" ]; then
+
+if ! [ "$X" = "$Y" ] && ! [ "$Y" = "no_specimen" ]; then
     echo "Output is $X whereas board is $Y"
+    exit
 fi
 if [ "$Y" = "sat" ]; then
     $PY verifier.py $INPUT $OUTPUT
-else
+elif [ "$Y" = "unsat" ]; then
     echo "Given solution is CORRECT"
+else
+    echo "Output is $X"
 fi
